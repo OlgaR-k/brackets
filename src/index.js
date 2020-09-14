381bc23434307be20425e0949374c574ff6f88b2
OlgaR-k <68254567+OlgaR-k@users.noreply.github.com>
2020-09-15 00:23:32 +0300

    init: start brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  let res = [];
+  let addElem;
+  for ( let i = 0; i < arr.length; i++ ) {
+    for ( let brackets of bracketsConfig ) {
+      if ( brackets.indexOf(arr[i]) == 0 ) {
+        res.splice(i, 0, arr[i], brackets[1]);
+        break;
+      }
+    }
+ }
+  return arr.join() == res.join();
 }
